7366e07b28dd977bf4758d6850b983431e59d644
roquiles <roberta_btg@hotmail.com>
2022-05-13 18:31:59 -0300

    Add prismic provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,25 @@
 import { AppProps } from 'next/app';
-import Header from '../components/Header';
 import '../styles/globals.scss';
+import Link from 'next/link';
+import { PrismicProvider } from '@prismicio/react';
+import { PrismicPreview } from '@prismicio/next';
+import { repositoryName, linkResolver } from '../services/prismic';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  );
 }
 
 export default MyApp;
