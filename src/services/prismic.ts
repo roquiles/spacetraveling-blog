714ece3be7c8821008fae6e65345c2bb132a747a
roquiles <roberta_btg@hotmail.com>
2022-05-13 18:32:33 -0300

    Include prismic config

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,18 +1,33 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import * as prismic from '@prismicio/client';
 import { HttpRequestLike } from '@prismicio/client';
 import { enableAutoPreviews } from '@prismicio/next';
+import sm from '../../sm.json';
+
+export const endpoint = sm.apiEndpoint;
+export const repositoryName = prismic.getRepositoryName(endpoint);
 
 export interface PrismicConfig {
   req?: HttpRequestLike;
 }
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export function linkResolver(doc) {
+  switch (doc.type) {
+    case 'posts':
+      return `/${doc.uid}`;
+    default:
+      return null;
+  }
+}
+
 export function getPrismicClient(config: PrismicConfig): prismic.Client {
-  const client = prismic.createClient(process.env.PRISMIC_API_ENDPOINT);
+  const client = prismic.createClient(endpoint);
 
   enableAutoPreviews({
     client,
     req: config.req,
-  })
+  });
 
   return client;
 }
